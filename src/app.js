50d79a528a12d102e46fe59617da699c9c3a2881
Akshar-mishra <mishraakshar71@gmail.com>
2026-04-21 02:31:15 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors({
     credential:true
 }))
 
-app.use(express.json({limit:'16kb'}))
+app.use(express.json({limit:'16kb'})) //allow our server to accept json data from frontend
 app.use(express.urlencoded({extended:true , limit:'16kb'}))
 app.use(express.static('public'))
 
